130ea605907aea32f52c9eab3ddeafed3af7a638
Cryptric <gawain.marti@cryptric.ch>
2022-05-07 18:05:37 +0200

    titel for overview

--- src/app/cardset-overview/cardset-overview.component.ts
+++ src/app/cardset-overview/cardset-overview.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 import { FlashcardSet } from '../model/flashcard-set';
 import { FlashcardService } from '../services/flashcard.service';
@@ -13,13 +14,14 @@ export class CardsetOverviewComponent implements OnInit {
   public flashcardSet!: FlashcardSet;
   public flashcardSetID!: number;
 
-  constructor(private flashcardService: FlashcardService, private route: ActivatedRoute) { }
+  constructor(private flashcardService: FlashcardService, private route: ActivatedRoute, private titleService: Title) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       this.flashcardSetID = params['id'];
       this.flashcardService.getFlashcardSet(this.flashcardSetID).subscribe(response => {
         this.flashcardSet = response.data;
+        this.titleService.setTitle("Tex-Cards " + this.flashcardSet!.flashcardSetName);
       }, error => {});
     });
   }
