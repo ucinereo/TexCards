170b3aeecaa2cc44e616ccad792b974f77e51f1f
Cryptric <gawain.marti@cryptric.ch>
2022-11-19 16:39:57 +0100

    removed none working api call

--- src/app/app/dashboard/dashboard.component.ts
+++ src/app/app/dashboard/dashboard.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { FlashcardsSets } from '../../model/flashcards-sets';
 import { FlashcardService } from '../../services/flashcard.service';
 import { CardsetCreationDialogComponent, CreationType } from '../cardset-creation-dialog/cardset-creation-dialog.component';
+import {FlashcardSet} from "../../model/flashcard-set";
 
 @Component({
   selector: 'app-dashboard',
@@ -12,62 +13,16 @@ import { CardsetCreationDialogComponent, CreationType } from '../cardset-creatio
 })
 export class DashboardComponent implements OnInit {
 
-  private dialogOpen: boolean = false;
-
-  public flashcardsSets?: FlashcardsSets;
-  public ownFlashcardSets?: FlashcardsSets;
+  public flashcardSets?: FlashcardSet[];
 
   constructor(private flashcardService: FlashcardService, public router: Router, private titleService: Title) { }
 
   ngOnInit(): void {
-    this.flashcardService.getFlashcardsSets().subscribe(response => {
-      this.flashcardsSets = response.data;
-      this.ownFlashcardSets = new FlashcardsSets();
-      this.flashcardsSets!.owner.forEach((element, index) => {
-        if (element) {
-          this.ownFlashcardSets!.ids.push(this.flashcardsSets!.ids[index]);
-          this.ownFlashcardSets!.names.push(this.flashcardsSets!.names[index]);
-          this.ownFlashcardSets!.writePermission.push(this.flashcardsSets!.writePermission[index]);
-          this.ownFlashcardSets!.owner.push(this.flashcardsSets!.owner[index]);
-        }
-      });
+    this.flashcardService.getFlashcardSets().subscribe(response => {
+
+      this.flashcardSets = response.data;
+
     }, (error) => { });
     this.titleService.setTitle("Tex-Cards " + "Flashcard sets")
   }
-
-  @HostListener('document:keydown.i')
-  openImportDialog(): void {
-    this.openDialog(CreationType.Import);
-  }
-
-  @HostListener('document:keydown.a')
-  openCreateDialog(): void {
-    this.openDialog(CreationType.Create);
-  }
-
-  private openDialog(creationType: CreationType) {
-    if (!this.dialogOpen) {
-      this.dialogOpen = true;
-      // const ref = this.dialog.open(CardsetCreationDialogComponent, {panelClass: 'creation-dialog', data: {type: creationType}});
-      //ref.afterClosed().subscribe(result => {
-      //  if (result && result.length > 0) {
-      //    this.createNewFlashcardSet(creationType, result);
-      //  }
-      //  this.dialogOpen = false;
-      //});
-    }
-  }
-
-
-  createNewFlashcardSet(creationType: CreationType, flashcardSetName: string): void {
-    this.flashcardService.createNewFlashcardSet(flashcardSetName).subscribe(response => {
-      if (creationType == CreationType.Create) {
-        this.router.navigate(['editor/' + response.data]);
-      } else {
-        this.router.navigate(['import/' + response.data]);
-      }
-    }, (error) => { });
-  }
-
-
 }
